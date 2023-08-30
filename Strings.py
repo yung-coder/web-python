e9e0e0d0e7b5c4f3200dd095411ba6aa155efe30
yung-coder <chandel.sanjeev5@gmail.com>
2023-08-30 21:03:36 +0530

    strings

---
+++ Strings.py
@@ -0,0 +1,23 @@
+name = "Kanye West is the goat"
+
+# length of the string
+
+print(len(name))
+
+# slicing 0 ---> 3 last will not be included
+
+print(name[0:4])
+
+# interpreted as len(name) -3 : len(name) - 1
+
+print(name[-3:-1])
+
+print(name.split(" "))
+
+# count the occurence
+
+print(name.count("Kanye"))
+
+# print(name.index("Jay-z"))
+
+# many other functions 
