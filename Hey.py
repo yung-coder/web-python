9f558586ef27d6b58d88f10de5b01504de3bc360
yung-coder <chandel.sanjeev5@gmail.com>
2023-08-30 20:28:13 +0530

    heypy

---
+++ Hey.py
@@ -0,0 +1,32 @@
+# ** is power off
+print(4**3)
+
+# Basic Print statement
+
+print("Hey", 6, 7, sep="~", end="009\n")
+print("Saksham")
+
+# Basic var
+
+a = 1
+print(a)
+
+# string concat
+
+a = "1"
+b = "2"
+print(a + b)
+
+# tyepcasting
+
+print(int(a) + int(b))
+
+# user input
+
+a = input("Enter your name")
+print("Name is", a)
+
+
+x = input("Number one");
+y = input("Enter two");
+print(int(x) + int(y));
