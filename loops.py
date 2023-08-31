6af056b9ac1d4ef4378fc8da03683abe456aac57
yung-coder <chandel.sanjeev5@gmail.com>
2023-08-31 23:18:34 +0530

    looping

---
+++ loops.py
@@ -0,0 +1,19 @@
+name = "Virgil"
+
+for i in name:
+    print(i)
+
+
+# range
+
+for k in range(5):
+    print(k)
+
+
+# while
+
+count = 5
+
+while count > 5:
+    print(count)
+    count = count - 1
