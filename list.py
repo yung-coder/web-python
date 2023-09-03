2b1a15b3e8ec622079f0394516b9683381868b50
yung-coder <chandel.sanjeev5@gmail.com>
2023-09-03 19:47:43 +0530

    list

---
+++ list.py
@@ -0,0 +1,15 @@
+marks = [3, 5, 6, "Kanye", True]
+
+print(marks[-3])
+print(marks[len(marks) - 3])
+
+
+if "6" in marks:
+    print("Yes")
+else:
+    print("No")
+
+
+lst = [ i for i in range(4)]
+
+print(lst)
