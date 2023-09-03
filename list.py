eae146568ba03c776ccab3169258c27cf5ad463c
yung-coder <chandel.sanjeev5@gmail.com>
2023-09-03 20:02:42 +0530

    tuple

--- list.py
+++ list.py
@@ -13,3 +13,16 @@ else:
 lst = [ i for i in range(4)]
 
 print(lst)
+
+# tuple 
+# tuple is  not mutable 
+
+tup = (1, 5 , 6)
+
+print(tup[0])
+
+
+tuple1 = (0,1, 2, 21 ,2, 3, 1, 3, 2, 3)
+
+res = tuple1.index(3, 4, 8)
+print('Count of 3 in tuple is' , res)
