d6a4cf0b6b3f31b8ae369d67ab42cd6cdc8ca796
yung-coder <chandel.sanjeev5@gmail.com>
2023-09-07 23:42:47 +0530

    sets and dict

--- sets.py
+++ sets.py
@@ -18,3 +18,25 @@ def square(n):
 
 square(5)
 print(square.__doc__)
+
+# sets
+
+info = {"jack", 14, 5, 19}
+print(info)
+
+for i in info:
+    print(i)
+
+
+# sets function
+
+s1 = {1, 2, 5, 6}
+s2 = {3, 6, 7}
+print(s1.intersection(s2))
+print(s1.union(s2))
+
+# Dictionaries
+
+dic = {"Chad": "Loser", "Car": "Lucid"}
+
+print(dic.keys())
