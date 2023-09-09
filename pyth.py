29129409f15ffbc5c1ea64ff8e296c3d367d44c0
yung-coder <chandel.sanjeev5@gmail.com>
2023-09-09 13:43:13 +0530

    unique stuff

--- pyth.py
+++ pyth.py
@@ -6,3 +6,29 @@ for i in range(6):
 # else tells when loop completes successfully 
 else: 
   print("Sorry on i")
+
+
+# Exception  handling 
+
+a = input("Enter no")
+
+try:
+   for value in range(1, 11):
+    print(value * int(a) )
+except Exception as e:
+    print("Inavlid")
+
+# Finally Clause  ---> while  returning function  
+
+finally: 
+    print("AOD")
+
+
+# Enumerate --> it gives the index  
+
+demo = [12, 13, 41 , 54, 67]
+
+for index , dem  in enumerate(demo):
+    print(index)
+    if (index == 3):
+         print("Hmmm")
