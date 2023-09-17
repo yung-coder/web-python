0a895abe1d4471a7fb4fc021f18956aadcea78fe
yung-coder <chandel.sanjeev5@gmail.com>
2023-09-17 13:54:47 +0530

    Inheritance

--- oop.py
+++ oop.py
@@ -132,3 +132,109 @@ print("The ID is:", Emp_1.id)
 print("The Name is:", Emp_1.name)
 print("The Address is:", Emp_1.Add)
 print("The Emails is:", Emp_1.Emails)
+
+
+# Inheritance
+
+# Multiple Inheritance 
+
+
+class Class1:
+	def m(self):
+		print("In Class1")
+	
+class Class2(Class1):
+	def m(self):
+		print("In Class2")
+
+class Class3(Class1):
+	def m(self):
+		print("In Class3")
+		
+class Class4(Class2, Class3):
+	pass
+	
+obj = Class4()
+obj.m()
+
+
+
+# Multilevel Inheritance 
+
+class Manager:
+	def final_review(self):
+		print("Final Review")
+ 
+class Reviewer(Manager):
+	def review(self):
+		print("Reviewing...")
+ 
+class Writer(Reviewer):
+	def writes(self):
+		print("Writes the code")
+ 
+o = Writer()
+o.final_review()
+o.review()
+o.writes()
+
+
+
+# Hybrid Inheritance
+
+
+
+class Office:
+
+    def func1(self):
+        print ('This function is in Office.')
+
+class Emp1(Office):
+
+    def func2(self):
+        print ('This function is in Employee 1.')
+
+class Emp2:
+
+    def func3(self):
+        print ('This function is in Employee 2.')
+
+class Emp3(Emp1, Emp2):
+
+    def func4(self):
+        print ('This function is in Employee 3.')
+
+# Driver's code
+
+object = Emp3()
+object.func1()
+object.func2()
+
+
+# Hierarchical Inheritance
+
+class A:
+
+    def function_1(self):
+        print ('Parent of B and C')
+
+# Derived class1
+
+class B(A):
+
+    def function_2(self):
+        print ('Child class of A')
+
+# Derivied class2
+
+class C(A):
+
+    def function_3(self):
+        print ('Child class of A')
+
+object1 = C()
+object2 = B()
+object1.function_1()
+object1.function_3()
+object2.function_1()
+object2.function_2()
