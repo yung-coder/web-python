f9ecee6f3c5c265c64c77bf77d83ba8582d6cb62
yung-coder <chandel.sanjeev5@gmail.com>
2023-09-18 23:22:17 +0530

    py13

--- extra.py
+++ extra.py
@@ -1,3 +1,5 @@
+import time
+
 x = [1, 2, 3, 4]
 
 print(dir(x))
@@ -5,6 +7,7 @@ print(dir(x))
 # print(help(str))
 
 # Dunder
+
 
 class Emp:
     def __init__(self, name):
@@ -18,32 +21,31 @@ e = Emp("Chad")
 print(str(e))
 
 
-# Method  Overriding 
+# Method  Overriding
 
 
 # Defining parent class
-class Parent():
-	
-	# Constructor
-	def __init__(self):
-		self.value = "Inside Parent"
-		
-	# Parent's show method
-	def show(self):
-		print(self.value)
-		
+class Parent:
+    # Constructor
+    def __init__(self):
+        self.value = "Inside Parent"
+
+    # Parent's show method
+    def show(self):
+        print(self.value)
+
+
 # Defining child class
 class Child(Parent):
-	
-	# Constructor
-	def __init__(self):
-		self.value = "Inside Child"
-		
-	# Child's show method
-	def show(self):
-		print(self.value)
-		
-		
+    # Constructor
+    def __init__(self):
+        self.value = "Inside Child"
+
+    # Child's show method
+    def show(self):
+        print(self.value)
+
+
 # Driver's code
 obj1 = Parent()
 obj2 = Child()
@@ -52,17 +54,33 @@ obj1.show()
 obj2.show()
 
 
-
-# Operator Overloading 
+# Operator Overloading
 
 print(1 + 2)
 
 # concatenate two strings
-print("Geeks"+"For")
+print("Geeks" + "For")
 
 # Product two numbers
 print(3 * 4)
 
 # Repeat the String
-print("Geeks"*4)
+print("Geeks" * 4)
 
+
+# Time Module
+
+print(4)
+time.sleep(3)
+print("This is on")
+
+
+# Walrus operator
+
+a = True
+print(a := False)
+
+foods = list()
+
+while (food := input("What food you like")) != "quit":
+    foods.append(food)
